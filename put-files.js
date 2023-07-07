a27b694a969c426d48483d772e746577d89969b3
Ericadhikari23 <glickflick3@gmail.com>
2023-07-07 10:15:21 +0545

    latest commit

--- put-files.js
+++ put-files.js
@@ -2,21 +2,17 @@ const { Web3Storage, getFilesFromPath, File } = require('web3.storage');
 const axios = require('axios');
 
 
-
-// contact information is an object with {email, phone and telephone number}
 class Company {
-    constructor(name, location, GLN_number, contactInformation, industrySector, dateOfEstablishment, ownership, description) {
-      this.name = name;
-      this.location = location;
-      this.GLN_number = GLN_number;
-      this.contactInformation = contactInformation;
-      this.industrySector = industrySector;
-      this.dateOfEstablishment = dateOfEstablishment;
-      this.ownership = ownership;
-      this.description = description;
-    }
+  constructor(name, location, contactInformation, industrySector, dateOfEstablishment, ownership, description) {
+    this.name = name;
+    this.location = location;
+    this.contactInformation = contactInformation;
+    this.industrySector = industrySector;
+    this.dateOfEstablishment = dateOfEstablishment;
+    this.ownership = ownership;
+    this.description = description;
   }
-
+}
 function getAccessToken(){
     return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDk3MzA3OTdDQTYxZjJmZTYyMjNhMTA1MTc0MDhBNDBFQmRBNjhhNGEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgzODU2NTYxMDksIm5hbWUiOiJTdXBwbHljaGFpbiJ9.GPDByGYycZ6S7SRvVpez2pbW7ImEnVSKSgy7uH_-OPw";
 }
@@ -25,22 +21,12 @@ function makeStorageClient () {
     return new Web3Storage({ token: getAccessToken() })
 }
 
-const comp1 = new Company("huzaf","bharatpur, chitwan",727636627,{
-  email : "hello@gmail",
-  phone : 9287772663,
-  tele : 01543672
-}, "Coorporate", "2061 BS", "private", "fake company just for test purpose");
-
-
-
-//console.log(comp1);
-
 function makeFileObjects (name) {
-    const obj = comp1 // here we pass the object of company 
+    const obj = name // here we pass the object of company 
     const buffer = Buffer.from(JSON.stringify(obj))
   
     const files = [
-      new File([buffer], `${name}.json`)
+      new File([buffer], `${name.name}.json`)
     ]
     return files
 }
@@ -48,15 +34,9 @@ function makeFileObjects (name) {
 async function storeFiles (files) {
     const client = makeStorageClient()
     const cid = await client.put(files)
-    console.log('stored files with cid:', cid)
+    //console.log('stored files with cid:', cid)
     return cid
 }
-
-
-//console.log(storeFiles(makeFileObjects(comp1.name)));
-
-
-// now retrive the fiels using the cid 
 
 
 async function retrieveFiles (cid) {
@@ -72,7 +52,8 @@ async function retrieveFiles (cid) {
         //arr.push([file.cid,file.name,file.size])
         const url = `http://${file.cid}.ipfs.w3s.link`;
 
-axios.get(url)
+
+        axios.get(url)
   .then(response => {
     console.log(response.data);
   })
@@ -83,13 +64,25 @@ axios.get(url)
     }
   }
 
-//retrieveFiles("bafybeicks5chhyahhdzyrassv7xdphjnvdbraulxll5tvonssujuhlmlky");
+async function main(){
+  const comp1 = new Company("huzaf","bharatpur, chitwan",727636627,{
+    email : "hello@gmail",
+    phone : 9287772663,
+    tele : 01543672
+  }, "Coorporate", "2061 BS", "private", "fake company just for test purpose");
+  const file_object = makeFileObjects(comp1);
+
+  const CID = await storeFiles(file_object);
+
+  retrieveFiles(CID);
+
+}
 
 
 
 
 
-
+main();
 
 
 
--- web3file.js
+++ web3file.js
@@ -1,50 +1,58 @@
 const Web3 = require('web3');
 const abii = require('./build/contracts/indentify.json')
 
-const address; 
-const private_key;
-const providerUrl = "https://eth-goerli.g.alchemy.com/v2/o8-xz0_My6osxn4HGVyxdtNdKMKggcn_";
-const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
-// contract address deploy garesi matra pauxa
-myContractAddress = abii.networks[5].address;
-var myContract = new web3.eth.Contract(abii.abi, myContractAddress);
 
 
-var isregistered = false;
-// ask to user 
-var has_account = true ;
+function initializeWeb3AndContract() {
+    const providerUrl = "https://eth-goerli.g.alchemy.com/v2/o8-xz0_My6osxn4HGVyxdtNdKMKggcn_";
+    const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
+  
+    const myContractAddress = abii.networks[5].address;
+    const myContract = new web3.eth.Contract(abii.abi, myContractAddress);
+  
+    return { web3, myContract };
+}
+    
 
-if(!has_account){
-    const account = web3.eth.accounts.create();
-    address = account.address;
-    private_key = account.privateKey;
-    console.log('New Account Created:');
-    console.log('Address:', account.address);
-    console.log('Private Key:', account.privateKey);
-    // log it somewhere may be to a local database 
-    // and make the has account to be true 
+function account_create_ifnot(has_account) {
+    const { web3, myContract } = initializeWeb3AndContract();
+	if (!has_account) {
+		const account = web3.eth.accounts.create();
+		address = account.address;
+		private_key = account.privateKey;
+		return {address , private_key};
+		// log it somewhere may be to a local database 
+		// and make the has account to be true 
+	}
 }
 
 
+// checking if the company for an address already exists 
+async function IsRegistered(Address){
+const { web3, myContract } = initializeWeb3AndContract();
 myContract.getPastEvents("CompanyRegistered",{fromBlock: 9291800, toBlock: "latest" }).then(function(events){
     for(const event of events){
         let data = event.returnValues;
-        if(data.ethAddress === "0x1A27464A985D1495088B642eEa1833e94171c3C3"){
-            isregistered = true;
-            console.log("yes this is registered")
+
+        console.log(data);
+        if(data.ethAddress === Address){
+            return true;
         }else{
-            console.log("no this is not registered")
+            return false;
         }
     }
 })
+}
 
 
-const send = async(ipfs_uri, isregistered ) =>{
-    const _from = address;
-    const privatekey = private_key;
+// here is a code to send transaction for registering the company
+const send = async(ipfs_uri, isregistered, private_keyy,Addresss) =>{
+    const { web3, myContract } = initializeWeb3AndContract();
+    const _from = Addresss;
+    const privatekey = private_keyy;
     const tx = {
         from : _from,
-        to : myContractAddress,
+        to : abii.networks[5].address,
         gas : 600000,
         data : myContract.methods.registerCompany(ipfs_uri,isregistered).encodeABI()
     }
@@ -58,12 +66,34 @@ const send = async(ipfs_uri, isregistered ) =>{
       });
 }
 
-//send("hellothisisafakeipfs", false);
 
-myContract.methods.getCompany("0x1A27464A985D1495088B542eEa1833e94171c3C3").call().then(result=>{
-    console.log(result)
-})
+async function get_uri_from_address(Address) {
+	const {
+		web3,
+		myContract
+	} = initializeWeb3AndContract();
+	myContract.methods.getCompany(Address).call().then(result => {
+		console.log(result)
+	})
+}
 
-// console.log(abii.abi);
 
 
+async function main(){
+    var has_account = false ; // ask the user if it us true or false then update this parameter by default it is false
+    var isregistered = false;
+    
+    const {Address , Private_key} = account_create_ifnot(has_account);
+    isregistered = await IsRegistered(address);
+
+    console.log("company registered : " , isregistered);
+    console.log("has account : ", has_account);
+    console.log("Account address : ", address);
+    console.log("Account Private Key : ", private_key);
+
+    const receipt = await send("fake uri",isregistered,Private_key,Address);
+    const getURI = await get_uri_from_address(address);
+
+}
+
+main();
